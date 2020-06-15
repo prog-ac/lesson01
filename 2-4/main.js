f9944c63eb08f489c450fae40dfed16ac6089f06
harukogenit <harukogenit@gmail.com>
2020-06-15 12:17:05 +0900

    季節

---
+++ 2-4/main.js
@@ -0,0 +1,11 @@
+const i = Number(process.argv[2])
+
+    if(i>=2 && i<=4){
+        console.log("春")
+    }else if(i>=5 && i<=7){
+     console.log("夏")
+    }else if(i>=8 && i<=10){
+     console.log("秋")
+    }else{
+     console.log("冬")  
+    }
