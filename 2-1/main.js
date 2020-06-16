bdd8a9b6c1af45be8373dc460a488fb3d3d4e668
makotoyasuda <>
2020-06-17 00:23:15 +0900

    create 2-1 question

---
+++ 2-1/main.js
@@ -0,0 +1,4 @@
+var NUM_1 = Number(process.argv[2]);
+var NUM_2 = Number(process.argv[3]);
+var ans = (NUM_1 / NUM_2) * 100;
+console.log(ans + "%");
---
+++ 2-1/main.ts
@@ -0,0 +1,6 @@
+
+const NUM_1:number = Number(process.argv[2]);
+const NUM_2:number = Number(process.argv[3]);
+
+let ans:number = (NUM_1 / NUM_2) * 100;
+console.log(`${ans}%`);
