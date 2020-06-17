393e2290800abc6661e763778e10824b08be6b9d
makotoyasuda <>
2020-06-17 19:33:53 +0900

    create 2-3 question

---
+++ 2-3/main.js
@@ -0,0 +1,3 @@
+var getHour = Number(process.argv[2]);
+var ans = getHour % 24;
+console.log(ans);
---
+++ 2-3/main.ts
@@ -0,0 +1,5 @@
+
+let getHour:number = Number(process.argv[2]);
+
+let ans:number = getHour % 24;
+console.log(ans);
