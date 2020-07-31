4b040da07a2e4402ef95a44d1e6da3f4dc1fb3cd
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-07-31 16:59:47 +0900

    for文の理解を深めて、正しいものを作成できました

--- 3-2/main.js
+++ 3-2/main.js
@@ -3,18 +3,20 @@ const horizontal = Number(process.argv[3]);
 
 
 
-for(let i = 0 , shape = ''; i < vertical; i++){
+for(let i = 0 ,shape = ''; i < vertical; i++){
 
   if(i % 2 !== 0){
     for(let m = 0 ; m < horizontal ; m++){
       shape += m % 2 !== 0 ? '*' : '-';
     }
     console.log(shape);
+    shape = '';
   } else {
     for(let m = 0 ; m < horizontal ; m++){
       shape += m % 2 === 0 ? '*' : '-';
     }
     console.log(shape);
+    shape = '';
   }
 }
 
