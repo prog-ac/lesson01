3b1dff9e35755a008114de45c83d8f26fa528e4c
kadaiho <d.murakami.gude@gmail.com>
2020-08-06 11:30:00 +0900

    1から50までの数字を表示する。ただし3と3の倍数のときだけWOWと表示するを実装しました。

---
+++ 4-2/main.js
@@ -0,0 +1,11 @@
+//1から50まで表示する
+for (i = 0; i < 50; i++) {
+  //  console.log(i + 1);
+  const str = String(i + 1);
+  //3の倍数と3がつく数字にWOWを表示する
+  if ((i + 1) % 3 === 0 || str.includes(`3`)) {
+    console.log(`WOW`);
+  } else {
+    console.log(i + 1);
+  }
+}
