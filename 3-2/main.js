0c15354e47220818d69d41fe2567b78d3b405553
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-11 09:34:19 +0900

    push,joinを使う方法でやってみました。

--- 3-2/main.js
+++ 3-2/main.js
@@ -1,22 +1,29 @@
 const vertical = Number(process.argv[2]);
 const horizontal = Number(process.argv[3]);
 
+let table = [];
 
 
 for(let i = 0 ,shape = ''; i < vertical; i++){
-
+  table = [];
   if(i % 2 !== 0){
     for(let m = 0 ; m < horizontal ; m++){
-      shape += m % 2 !== 0 ? '*' : '-';
+      if(m % 2 !== 0){
+        table.push('*');
+      } else {
+        table.push('-');
+      }
     }
-    console.log(shape);
-    shape = '';
+    console.log(table.join(''));
   } else {
     for(let m = 0 ; m < horizontal ; m++){
-      shape += m % 2 === 0 ? '*' : '-';
+      if(m % 2 === 0){
+        table.push('*');
+      } else {
+        table.push('-');
+      }
     }
-    console.log(shape);
-    shape = '';
+    console.log(table.join(''));
   }
 }
 
