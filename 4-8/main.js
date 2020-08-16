0d72cd42c4fb918b751afd5b1e4945fbb814c091
kadaiho <d.murakami.gude@gmail.com>
2020-08-16 12:46:04 +0900

    探索を実装しました。

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,20 @@
-const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+const str = "こはるはるここはるここはるはるここはるこはるはるはる";
+
+const word = `こはる`;
+const word2 = `はるこ`;
+const number = `こはる`.length;
+let counts = 0;
+let counts2 = 0;
+
+for (let i = 0; i < str.length; i++) {
+  let str2 = str[i];
+  for (let a = 1; a < number; a++) {
+    str2 += str[i + a];
+  }
+  if (str2 == word) {
+    counts++;
+  } else if (str2 == word2) {
+    counts2++;
+  }
+}
+console.log(`こはる：${counts}\nはるこ:${counts2}`);
