b5b9061c2a79a9e27995ebf731a0631f7332fea3
kadaiho <d.murakami.gude@gmail.com>
2020-08-16 13:52:59 +0900

    探索を修正しました。

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,19 +1,19 @@
 const str = "こはるはるここはるここはるはるここはるこはるはるはる";
 
-const word = `こはる`;
-const word2 = `はるこ`;
-const number = `こはる`.length;
+const searchingWord = `こはる`;
+const searchingWord2 = `はるこ`;
+const searchingWordCount = `こはる`.length;
 let counts = 0;
 let counts2 = 0;
 
 for (let i = 0; i < str.length; i++) {
-  let str2 = str[i];
-  for (let a = 1; a < number; a++) {
-    str2 += str[i + a];
+  let selectingWord = str[i];
+  for (let a = 1; a < searchingWordCount; a++) {
+    selectingWord += str[i + a];
   }
-  if (str2 == word) {
+  if (selectingWord == searchingWord) {
     counts++;
-  } else if (str2 == word2) {
+  } else if (selectingWord == searchingWord2) {
     counts2++;
   }
 }
